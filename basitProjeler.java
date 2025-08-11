dc27bb09006b9bb60f88a4d0eebc0d2a2c99b5df
esmabru <esmaebruozturk@gmail.com>
2025-08-11 15:54:01 +0300

    Update and rename TekCiftKontrol.java to basitProjeler.java

--- basitProjeler.java
+++ basitProjeler.java
@@ -26,3 +26,29 @@ public class merkez{
 		girdi.close();
    }
 }
+
+//Girilen 3 sayıdan en büyüğünü bulan program
+import java.util.Scanner;
+
+public class merkez {
+	public static void main(String[] args) {
+		Scanner girdi= new Scanner(System.in);
+		
+		System.out.print("İlk sayıyı giriniz:");
+		int sayi1=girdi.nextInt();
+		System.out.print("İkinci sayıyı giriniz:");
+		int sayi2=girdi.nextInt();
+		System.out.print("Üçüncü sayıyı giriniz:");
+		int sayi3=girdi.nextInt();
+		
+		if(sayi1>sayi2 && sayi1>sayi3) {
+		System.out.println("En büyük sayı "+sayi1);
+	}
+		else if(sayi2>sayi1 && sayi2>sayi3) {
+			System.out.println("En büyük sayı "+sayi2);
+		}
+		else {
+			System.out.println("En büyük sayı "+sayi3);
+		}
+   }
+}
